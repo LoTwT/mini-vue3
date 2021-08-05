78cb7aa294c3f024a99fddd2e46bffe1fe51f27a
LoTwT <709937065@qq.com>
2021-08-05 11:27:47 +0800

    deal props

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -21,11 +21,7 @@ const mount = (vnode, container) => {
             const value = vnode.props[key]
 
             // 边界条件处理：事件 or 属性
-            if (key.startsWith("on")) {
-                el.addEventListener(key.slice(2).toLowerCase(), value)
-            } else {
-                el.setAttribute(key, value)
-            }
+            dealProp(el, key, value)
         }
     }
 
@@ -41,4 +37,62 @@ const mount = (vnode, container) => {
 
     // 4. 将 el 挂载到 container 上
     container.appendChild(el)
+}
+
+/**
+ * 处理 props 中的单个键值对
+ * 边界条件处理：事件 or 属性
+ */
+const dealProp = (el, key, value, isAdd = true) => {
+    if (key.startsWith("on")) {
+        if (isAdd) el.addEventListener(key.slice(2).toLowerCase(), value)
+        else el.removeEventListener(key.slice(2).toLowerCase(), value)
+    } else {
+        if (isAdd) el.setAttribute(key, value)
+        else el.removeAttribute(key)
+    }
+}
+
+/**
+ * 
+ * @param n1 原 vnode
+ * @param n2 新 vnode
+ */
+const patch = (n1, n2) => {
+    // 当两个 vnode 的 tag 不同时，直接移除原 vnode，挂载新的 vnode
+    if (n1.tag !== n2.tag) {
+        // 获得 n1 的父元素
+        const n1ElParent = n1.el.parentElement
+        // 移除 n1
+        n1ElParent.removeChild(n1.el)
+        // 挂载 n2
+        mount(n2, n1ElParent)
+    } else {
+        // 两个 vnode 的 tag 相同时
+        // 1. 取出 element 对象，并在 n2 中进行保存
+        const el = n2.el = n1.el
+
+        // 2. 处理 props
+        const oldProps = n1.props || {}
+        const newProps = n2.props || {}
+
+        // 2.1 获取所有的 newProps 添加到 el
+        for (const key in newProps) {
+            const oldValue = oldProps[key]
+            const newValue = newProps[key]
+
+            if (newValue !== oldValue) {
+                dealProp(el, key, newValue)
+            }
+        }
+
+        // 2.2 删除旧的 props
+        for (const key in oldProps) {
+            if (!(key in newProps)) {
+                dealProp(el, key, oldProps[key], false)
+            }
+        }
+
+        // 3. 处理 children
+    }
 }
