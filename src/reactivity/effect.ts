9b2b17bbd9c0433130229056d7c853fbd8975075
LoTwT <709937065@qq.com>
2021-10-03 15:54:44 +0800

    feat: effect & reactive & track & trigger & return runner

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,50 @@
+class ReactiveEffect {
+  private _fn: Function
+  constructor(fn: Function) {
+    this._fn = fn
+  }
+  run() {
+    activeEffect = this
+    return this._fn()
+  }
+}
+
+let activeEffect
+export const effect = (fn) => {
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+
+  return _effect.run.bind(_effect)
+}
+
+// 依赖收集容器 Map<target, Map<key, dep>>
+const targetMap = new Map<any, Map<any, Set<ReactiveEffect>>>()
+
+// 依赖收集
+export const track = (target, key) => {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+
+  dep.add(activeEffect)
+}
+
+// 触发依赖
+export const trigger = (target, key) => {
+  const depsMap = targetMap.get(target)
+  const depSet = depsMap?.get(key)
+
+  if (depSet) {
+    depSet.forEach((dep) => dep.run())
+  } else {
+    throw Error("empty depSet")
+  }
+}
--- src/reactivity/tests/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import { add } from "../index"
-
-it("init", () => {
-  expect(true).toBe(true)
-})
