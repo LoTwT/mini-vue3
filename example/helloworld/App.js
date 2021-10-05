b9492a528d0ea799ea07cfcfa86b01451a66157b
LoTwT <709937065@qq.com>
2021-10-05 19:03:15 +0800

    feat(runtime-core): processElement main process

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,16 @@ import { h } from "../../lib/mini-vue.esm.js"
 export const App = {
   render() {
     // 视图逻辑 (ui 逻辑)
-    return h("div", `hello, ${this.msg}`)
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "hard"],
+      },
+      // `hello, ${this.msg}`,
+      // `hello, mini-vue`,
+      [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")],
+    )
   },
   setup() {
     // composition api
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -7,17 +8,48 @@ export function render(vnode, container) {
 
 export function patch(vnode, container) {
   // todo 判断是 component 还是 element
-  // processElement()
-
-  // 处理 component
-  processComponent(vnode, container)
+  if (typeof vnode.type === "string") {
+    // 处理 element
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    // 处理 component
+    processComponent(vnode, container)
+  }
 }
 
-export function processComponent(vnode, container) {
+function processElement(vnode, container) {
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+  const { type, props, children } = vnode
+  // type
+  const el = document.createElement(type)
+
+  // props
+  for (const key in props) {
+    el.setAttribute(key, props[key])
+  }
+
+  // children -> Array String
+  if (typeof children === "string") {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+  }
+
+  container.append(el)
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => patch(v, container))
+}
+
+function processComponent(vnode, container) {
   mountComponent(vnode, container)
 }
 
-export function mountComponent(vnode, container) {
+function mountComponent(vnode, container) {
   const instance = createComponentInstance(vnode)
 
   setupComponent(instance)
