640cb9a64010724e18355062a560ea9fd4d79c5c
LoTwT <709937065@qq.com>
2021-10-24 14:47:06 +0800

    feat(runtime-core): emit

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,6 +1,7 @@
 import { PublicInstanceProxyHandlers } from "./componentPublicInstance"
 import { initProps } from "./componentProps"
 import { shallowReadonly } from "../reactivity/reactive"
+import { emit } from "./componentEmit"
 
 export function createComponentInstance(vnode) {
   const component = {
@@ -8,7 +9,10 @@ export function createComponentInstance(vnode) {
     type: vnode.type,
     setupState: {},
     props: {},
+    emit: (event) => {},
   }
+
+  component.emit = emit.bind(null, component)
 
   return component
 }
@@ -29,7 +33,9 @@ function setupStatefulComponent(instance) {
   if (setup) {
     // 如果是 function，则认为是 render 函数
     // 如果是 object，注入到组件上下文中
-    const setupResult = setup(shallowReadonly(instance.props))
+    const setupResult = setup(shallowReadonly(instance.props), {
+      emit: instance.emit,
+    })
 
     handleSetupResult(instance, setupResult)
   }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -6,3 +6,13 @@ export const hasChanged = (value, newValue) => !Object.is(value, newValue)
 
 export const hasOwn = (val, key) =>
   Object.prototype.hasOwnProperty.call(val, key)
+
+// add -> Add
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1)
+
+// add-foo -> addFoo
+export const camelize = (str: string) =>
+  str.replace(/-(\w)/g, (_, c: string) => (c ? c.toUpperCase() : ""))
+
+export const toHandlerKey = (str: string) => (str ? "on" + capitalize(str) : "")
