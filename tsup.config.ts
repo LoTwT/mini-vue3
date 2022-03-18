5836ee26cc6e724d34cc92d08b9fc319e082f016
LoTwT <709937065@qq.com>
2022-03-18 22:49:10 +0800

    chore: add tsup & build monorepo

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from "tsup"
+import { defineConfig, Options } from "tsup"
+import fg from "fast-glob"
 
-export default defineConfig({
-  entry: ["packages/*/index.ts"],
-  format: ["esm", "cjs"],
-  minify: true,
-})
+const packageNames = fg
+  .sync("packages/*", { onlyDirectories: true })
+  .map((p) => p.split("/")[1])
+
+export default defineConfig([
+  {
+    name: "mini-vue3",
+    entry: ["packages/**/src/index.ts"],
+    format: ["esm", "cjs"],
+    minify: true,
+    target: "node16",
+  },
+  ...packageNames.map(
+    (n): Options => ({
+      name: n,
+      entry: [`packages/${n}/src/index.ts`],
+      format: ["esm", "cjs"],
+      minify: true,
+      outDir: `packages/${n}/dist`,
+      target: "node16",
+    }),
+  ),
+])
