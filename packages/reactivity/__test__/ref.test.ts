f28bbd7532c71b24f4527ee4bede3f6639441be9
LoTwT <709937065@qq.com>
2022-03-26 22:59:17 +0800

    feat(reactivity): 18 实现 proxyRefs

--- packages/reactivity/__test__/ref.test.ts
+++ packages/reactivity/__test__/ref.test.ts
@@ -1,6 +1,6 @@
 import { effect } from "../src/effect"
 import { reactive } from "../src/reactive"
-import { isRef, ref, unRef } from "../src/ref"
+import { isRef, proxyRefs, ref, unRef } from "../src/ref"
 
 describe("ref", () => {
   it("happy path", () => {
@@ -58,5 +58,26 @@ describe("ref", () => {
 
     expect(unRef(a)).toBe(1)
     expect(unRef(1)).toBe(1)
+  })
+
+  it("proxyRefs", () => {
+    // 用于模板自动解包
+    const user = {
+      age: ref(10),
+      name: "xiaohong",
+    }
+
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(10)
+    expect(proxyUser.age).toBe(10)
+    expect(proxyUser.name).toBe("xiaohong")
+
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(30)
+    expect(proxyUser.age).toBe(30)
+    expect(user.age.value).toBe(30)
   })
 })
