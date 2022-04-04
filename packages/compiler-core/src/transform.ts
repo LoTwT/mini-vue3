449a15c9cbc681e1c3195b4941e56beb58d9d449
LoTwT <709937065@qq.com>
2022-04-04 16:37:54 +0800

    feat(compiler-core): 实现代码生成 string 类型

--- packages/compiler-core/src/transform.ts
+++ packages/compiler-core/src/transform.ts
@@ -1,9 +1,14 @@
-export function transform(root, options) {
+export function transform(root, options = {}) {
   const context = createTransformContext(root, options)
 
-  // 1. 遍历 - 深度优先搜索
-  // 2. 修改 text content
+  // 遍历 - 深度优先搜索
   traverseNode(root, context)
+
+  createRootCodegen(root)
+}
+
+function createRootCodegen(root) {
+  root.codegenNode = root.children[0]
 }
 
 function createTransformContext(root, options) {
